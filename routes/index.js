5bfa5f9c56d4b5807534e9ef8462bb6e0597c0a3
Rycer <szymkris5@gmail.com>
2024-05-14 14:13:18 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi",
+    user: "Rycer",
+    added: new Date(),
+  },
+  {
+    text: "Hello",
+    user: "Syrax",
+    added: new Date(),
+  },
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { messages });
 });
 
+router.get("/new", function(req, res, next) {
+  res.render("form");
+});
+router.post("/new", function(req, res, next) {
+  const message = req.body;
+  messages.push({ text: message.text, user: message.user, added: new Date() });
+  res.redirect("/");
+});
 module.exports = router;
